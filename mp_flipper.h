8e61678fef8f679a85c70c3c1d08d8c5a39c2894
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-04-05 08:00:52 +0200

    add init and exec str wrappers

--- mp_flipper.h
+++ mp_flipper.h
@@ -1,5 +1,11 @@
-# pragma once
+#pragma once
+
+#include <stddef.h>
 
 extern const char* mp_flipper_root_module_path;
 
-void mp_flipper_set_root_module_path(const char *path);
+void mp_flipper_set_root_module_path(const char* path);
+
+void mp_flipper_init(void* gc_heap, size_t gc_heap_size, void* stack_top);
+void mp_flipper_exec_str(const char* str);
+void mp_flipper_deinit();
