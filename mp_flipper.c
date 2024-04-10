4af3b8536e21f985d2e8c43365a403f78b1af36c
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-04-10 04:47:13 +0200

    library update

--- mp_flipper.c
+++ mp_flipper.c
@@ -97,32 +97,41 @@ void mp_flipper_exec_mpy_file(const char* file_path) {
 }
 
 void mp_flipper_compile_and_save_file(const char* py_file_path, const char* mpy_file_path) {
-    mp_lexer_t* lex = mp_lexer_new_from_file(qstr_from_str(py_file_path));
+    nlr_buf_t nlr;
 
-    qstr source_name = qstr_from_str(lex->source_name);
+    if(nlr_push(&nlr) == 0) {
+        mp_lexer_t* lex = mp_lexer_new_from_file(qstr_from_str(py_file_path));
 
-    mp_store_global(MP_QSTR___file__, MP_OBJ_NEW_QSTR(lex->source_name));
+        qstr source_name = qstr_from_str(lex->source_name);
 
-    mp_parse_tree_t parse_tree = mp_parse(lex, MP_PARSE_FILE_INPUT);
-    mp_compiled_module_t cm;
-    cm.context = m_new_obj(mp_module_context_t);
-    mp_compile_to_raw_code(&parse_tree, lex->source_name, false, &cm);
+        mp_store_global(MP_QSTR___file__, MP_OBJ_NEW_QSTR(lex->source_name));
 
-    mp_print_t* print = malloc(sizeof(mp_print_t));
+        mp_parse_tree_t parse_tree = mp_parse(lex, MP_PARSE_FILE_INPUT);
+        mp_compiled_module_t cm;
+        cm.context = m_new_obj(mp_module_context_t);
+        mp_compile_to_raw_code(&parse_tree, lex->source_name, false, &cm);
 
-    print->data = mp_flipper_print_data_alloc();
-    print->print_strn = mp_flipper_print_strn;
+        mp_print_t* print = malloc(sizeof(mp_print_t));
 
-    mp_raw_code_save(&cm, print);
+        print->data = mp_flipper_print_data_alloc();
+        print->print_strn = mp_flipper_print_strn;
 
-    const char* data = mp_flipper_print_get_data(print->data);
-    size_t size = mp_flipper_print_get_data_length(print->data);
+        mp_raw_code_save(&cm, print);
 
-    mp_flipper_save_file(mpy_file_path, data, size);
+        const char* data = mp_flipper_print_get_data(print->data);
+        size_t size = mp_flipper_print_get_data_length(print->data);
 
-    mp_flipper_print_data_free(print->data);
+        mp_flipper_save_file(mpy_file_path, data, size);
 
-    free(print);
+        mp_flipper_print_data_free(print->data);
+
+        free(print);
+
+        nlr_pop();
+    } else {
+        // Uncaught exception: print it out.
+        mp_obj_print_exception(&mp_plat_print, (mp_obj_t)nlr.ret_val);
+    }
 }
 
 void mp_flipper_deinit() {
