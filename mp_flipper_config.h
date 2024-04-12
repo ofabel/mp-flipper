25fbaeeca707e688104aaedd5165729f1ece019e
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-04-12 05:09:46 +0200

    library update

--- mp_flipper_config.h
+++ mp_flipper_config.h
@@ -1,7 +1,9 @@
+#pragma once
+
 #include <stdint.h>
 
 #ifndef MP_FLIPPER_IS_COMPILER
-#define MP_FLIPPER_IS_COMPILER (0)
+#define MP_FLIPPER_IS_COMPILER (1)
 #endif
 #ifndef MP_FLIPPER_IS_RUNTIME
 #define MP_FLIPPER_IS_RUNTIME (1)
---
+++ mp_flipper_config_compiler.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef MP_FLIPPER_IS_COMPILER
+#define MP_FLIPPER_IS_COMPILER (1)
+#endif
+#ifndef MP_FLIPPER_IS_RUNTIME
+#define MP_FLIPPER_IS_RUNTIME (0)
+#endif
+
+#include "mp_flipper_config.h"
