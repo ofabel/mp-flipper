cfa30b7571d7f2b6f64939d113249b6fb1187c57
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-04-12 08:04:01 +0200

    library update

--- mpconfigport.h
+++ mpconfigport.h
@@ -1,4 +1,9 @@
+#ifndef MP_FLIPPER_IS_COMPILER
 #define MP_FLIPPER_IS_COMPILER defined(MP_FLIPPER_COMPILER)
+#endif
+
+#ifndef MP_FLIPPER_IS_RUNTIME
 #define MP_FLIPPER_IS_RUNTIME defined(MP_FLIPPER_RUNTIME)
+#endif
 
 #include "mp_flipper_config.h"
