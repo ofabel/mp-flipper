636fdec342f509d8cb82a2d5c1bd4c29119280d2
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-04-14 16:52:00 +0200

    remove obsolete files

--- mp_flipper_app.c
+++ mp_flipper_app.c
@@ -5,14 +5,51 @@
 #include <storage/storage.h>
 
 #include <mp_flipper_runtime.h>
+#include <mp_flipper_compiler.h>
 
-#include "mp_flipper_app.h"
-#include "py_app.h"
+#define TAG "uPython"
 
-const char* root_module_path;
+static void execute_file(FuriString* file) {
+    size_t stack;
 
-ViewPort* mp_flipper_view_port;
-FuriMessageQueue* mp_flipper_event_queue;
+    const char* path = furi_string_get_cstr(file);
+    FuriString* file_path = furi_string_alloc_printf("%s", path);
+
+    do {
+        FURI_LOG_I(TAG, "executing script %s", path);
+
+        const size_t heap_size = memmgr_get_free_heap() * 0.1;
+        const size_t stack_size = 2 * 1024;
+        uint8_t* heap = malloc(heap_size * sizeof(uint8_t));
+
+        FURI_LOG_D(TAG, "initial heap size is %zu bytes", heap_size);
+        FURI_LOG_D(TAG, "stack size is %zu bytes", stack_size);
+
+        size_t index = furi_string_search_rchar(file_path, '/');
+
+        furi_check(index != FURI_STRING_FAILURE);
+
+        bool is_py_file = furi_string_end_with_str(file_path, ".py");
+
+        furi_string_left(file_path, index);
+
+        mp_flipper_set_root_module_path(furi_string_get_cstr(file_path));
+
+        mp_flipper_init(heap, heap_size, stack_size, &stack);
+
+        if(is_py_file) {
+            mp_flipper_exec_py_file(path);
+        } else {
+            mp_flipper_exec_mpy_file(path);
+        }
+
+        mp_flipper_deinit();
+
+        free(heap);
+    } while(false);
+
+    furi_string_free(file_path);
+}
 
 static bool select_python_file(FuriString* file_path) {
     DialogsApp* dialogs = furi_record_open(RECORD_DIALOGS);
@@ -37,7 +74,7 @@ int32_t mp_flipper_app(void* p) {
     FuriString* file_path = furi_string_alloc();
 
     if(select_python_file(file_path)) {
-        py_app_file_execute(file_path);
+        execute_file(file_path);
     }
 
     furi_string_free(file_path);
