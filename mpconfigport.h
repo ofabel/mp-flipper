2ab2d943a711b08220b89ee303cfce3b04229260
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-04-15 19:18:55 +0200

    library update

--- mpconfigport.h
+++ mpconfigport.h
@@ -1,9 +1,10 @@
 #define MP_FLIPPER_COMPILER
 #define MP_FLIPPER_RUNTIME
-//#define MP_FLIPPER_MPY_SUPPORT
+#define MP_FLIPPER_MPY_SUPPORT
 #define MP_FLIPPER_FIRMWARE
-//#define MP_FLIPPER_MATH
-//#define MP_FLIPPER_JSON
-//#define MP_FLIPPER_IO
+#define MP_FLIPPER_SPLIT_HEAP
+#define MP_FLIPPER_MATH
+#define MP_FLIPPER_JSON
+#define MP_FLIPPER_IO
 
 #include "mp_flipper_config.h"
